a07acc302602f0f5e01c3293e49709664d4563bc
Alfonso Enriquez Castro <alfonso.en.ca@gmail.com>
2015-08-13 11:25:49 -0400

    first commit

---
+++ server/db/models/route.js
@@ -0,0 +1,38 @@
+'use strict';
+var mongoose = require('mongoose');
+
+var schema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true
+	},
+	url: {
+		type: String,
+		required: true
+	},
+	data: [{
+		name: String,
+		selectors: String,
+		cached: String
+	}],
+	crawlFrequency: {
+		// stored as milliseconds - consistent with Unix time
+		type: Number,
+		required: true
+	},
+	lastTimeCrawled: {
+		type: Date
+	},
+	lastCrawlStatus: {
+		// true if successful, false if failed
+		type: Boolean,
+		default: true
+	},
+	count: {
+		// times crawled
+		type: Number,
+		default: 0
+	}
+});
+
+mongoose.model('Route', schema);

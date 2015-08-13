d501e7bca15848bd95e5722f324ca3b5a6c71ccb
Jack Mulrow <jackmulrow@Jacks-MacBook-Pro.local>
2015-08-13 12:38:26 -0400

    post route for new routes

--- server/app/routes/routes/index.js
+++ server/app/routes/routes/index.js
@@ -4,8 +4,13 @@ var router = require('express').Router();
 var mongoose = require('mongoose');
 var Route = mongoose.model('Route');
 
-router.get('/testing', function(req, res, next) {
-    Route.findOne({}).exec()
+// return crawled data for a route
+router.get('/:userKey/:routeName', function(req, res, next) {
+	// do validation with the userkey
+    Route.findOne({
+    	name: req.params.routeName,
+    	userKey: req.params.userKey
+    }).exec()
         .then(function(route) {
             return route.getCrawlData();
         })
@@ -13,6 +18,22 @@ router.get('/testing', function(req, res, next) {
             res.json(crawledData);
         })
         .then(null, next);
+});
+
+// create a new route (userKey and routeName in the body)
+router.post('/', function(req, res, next) {
+	// req.body should have name, userKey, url, data, and config
+	var newRoute = new Route(req.body);
+	newRoute.save()
+		.then(function(route) {
+			// res.json(route);
+			// return the crawled data
+			return route.getCrawlData();
+		})
+		.then(function(data) {
+			res.json(data);
+		})
+		.then(null, next);
 });
 
 

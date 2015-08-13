d15720e885268df5eb68d38864fcd33c239f3978
Jack Mulrow <jackmulrow@Jacks-MacBook-Pro.local>
2015-08-13 11:59:17 -0400

    finished crawler and attached it to route model

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -3,6 +3,8 @@ var router = require('express').Router();
 module.exports = router;
 
 router.use('/members', require('./members'));
+// router.use('/users', require('./users'));
+router.use('/routes', require('./routes'));
 
 // Make sure this is after all of
 // the registered routes!

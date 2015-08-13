1e8b403d0e19ac0a1755adfbd069eb59a9a3d17f
thang239 <thanghnbk@gmail.com>
2015-08-13 18:10:55 -0400

    selectorgadget works but unstable

--- ce_nodemono/nodemono.js
+++ ce_nodemono/nodemono.js
@@ -12,25 +12,24 @@
 // ang.setAttribute("scr", "https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js")
 // document.getElementsByTagName("head")[0].appendChild(ang);
 //create link element contain some custom styles
-var css = document.createElement("link");
-css.setAttribute("rel", "stylesheet");
-css.setAttribute("type", "text/css");
-css.setAttribute("href", chrome.extension.getURL("css/style.css"));
-document.getElementsByTagName("head")[0].appendChild(css);
 
-//create link element contain bootstrap library
-var bootstrap = document.createElement("link");
-bootstrap.setAttribute("rel", "stylesheet");
-bootstrap.setAttribute("type", "text/css");
-bootstrap.setAttribute("href", chrome.extension.getURL("css/bootstrap.min.css"));
-document.getElementsByTagName("head")[0].appendChild(bootstrap);
+importCSS(chrome.extension.getURL("css/style.css"));
+importCSS(chrome.extension.getURL("css/bootstrap.min.css"))
+importCSS(chrome.extension.getURL("selectorgadget/selectorgadget.css"))
+importJS(chrome.extension.getURL("jquery.min.js"))
+console.log(jQuery);
+jQuery.noConflict();
+importJS(chrome.extension.getURL("selectorgadget/diff_match_patch.js"))
+importJS(chrome.extension.getURL("selectorgadget/dom.js"))
+// importJS(chrome.extension.getURL("selectorgadget/dom.js"))
+importJS(chrome.extension.getURL("selectorgadget/interface.js"))
 
 function transformHtml() {
-	$
+	// $
 }
 
-
-
+// window.selectorgadget = new SelectorGadget();
+// console.log(window.selectorgadget);
 $.get(chrome.extension.getURL('kimono-toolbar.html'), function(data) {
 	// console.log(data);
 	// $(data).appendTo('body');
@@ -82,3 +81,25 @@ $.get(chrome.extension.getURL('kimono-toolbar.html'), function(data) {
 	// console.log(angular);
 	console.log('Boot and loaded !');
 });
+
+
+
+function importJS(src) {
+	var script = document.createElement("script");
+	script.setAttribute("type", "text/javascript");
+	script.setAttribute("src", src);
+	document.getElementsByTagName("head")[0].appendChild(script);
+
+	// $('head').appendChild(script);
+}
+
+function importCSS(href) {
+	var css = document.createElement("link");
+	css.setAttribute("rel", "stylesheet");
+	css.setAttribute("type", "text/css");
+	css.setAttribute("href", href);
+	document.getElementsByTagName("head")[0].appendChild(css);
+
+
+
+}

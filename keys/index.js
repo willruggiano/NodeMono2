4ab636823808c5258477385f7ced3a27b380fcc8
Jack Mulrow <jackmulrow@Jacks-MacBook-Pro.local>
2015-08-24 17:16:28 -0400

    moved https and oauth keys to keys folder - git ignore when everyone has downloaded it

---
+++ keys/index.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+module.exports = {
+	google: {
+		clientID: '366183496221-laqlnvl94llgrn43fsuupgh2jlgsvs4d.apps.googleusercontent.com',
+		clientSecret: 'gaQgopM8BZ5G9HPX_lKWHy6F',
+		callbackURL: 'http://localhost:1337/auth/google/callback'
+	},
+	github: {
+		clientID: '5d800ac60952e6c080aa',
+		clientSecret: 'ab3889fef138eba662776f935c597900711f9820',
+		callbackURL: 'http://localhost:1337/auth/github/callback'
+	},
+	https: {
+		cert: fs.readFileSync(__dirname + '/cert.pem'),
+		key: fs.readFileSync(__dirname + '/key.pem')
+	}
+};

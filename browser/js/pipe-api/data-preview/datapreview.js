9e73f7434a2f493b6d09751ac0b31be4e41bdbab
Will Ruggiano <will@ruggianofamily.com>
2015-08-26 13:26:17 -0400

    style changes

--- browser/js/pipe-api/data-preview/datapreview.js
+++ browser/js/pipe-api/data-preview/datapreview.js
@@ -26,7 +26,7 @@ app.config(($stateProvider) => {
           };
         };
 
-      $scope.resultTypes = ["CSV", "RSS", "JSON"];
+      // $scope.resultTypes = ["CSV", "RSS", "JSON"];
       $scope.activeResultType = "CSV";
 
       $scope.setActiveType = (type) =>{
